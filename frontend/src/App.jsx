b388a013f82ad4685af9e09301ac8ef9037e23b8
mukhunth <69753475+mukhunth@users.noreply.github.com>
2026-07-31 10:15:25 +0530

    Scaffold express backend and vite react frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import {BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen font-sans text-gray-900 bg-gray-50">
+        <h1>React test</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
